71390e3c767a73e27ebf5c9569e894d3de8845e2
Ashutosh1910 <desaiashutosh1910@gmail.com>
2025-06-06 23:38:14 +0530

    added p to p collision

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 build:
-	eval g++ main.cpp  -framework IOKit -framework Cocoa -framework OpenGL $(pkg-config --libs --cflags raylib) -o main
+	g++ main.cpp  -framework IOKit -framework Cocoa -framework OpenGL $(shell pkg-config --libs --cflags raylib) -o main
 run:
 	./main
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include "raylib.h"
+#include "raymath.h"
 using namespace std;
 #define WALL_WIDTH 5
 #define WALL_HEIGHT 5
@@ -18,7 +19,7 @@ class Ball
         Vector2 velocity;
         Vector2 acceleration;
         float radius;
-        int mass;
+        float mass=1;
         Ball(int x,int y,float radius,float velocityX,float velocityY){
             this->pos=(Vector2){(float)x,(float)y};
             this->velocity=(Vector2){(float)velocityX,(float)velocityY};
@@ -36,6 +37,7 @@ bool checkCollision(Ball* b1,Ball* b2){
 }
 
 void UpdateBalls(vector<Ball*> balls,float dt){
+    if(balls.empty()) return;
     for(auto& b: balls){
         b->pos={
             .x=b->pos.x+b->velocity.x*dt,
@@ -44,9 +46,41 @@ void UpdateBalls(vector<Ball*> balls,float dt){
     }
     //ball to ball collision
     for(int i=0;i<balls.size()-1;i++){
-        for(int j=0;j<balls.size();j++){
+        for(int j=i+1;j<balls.size();j++){
             if(checkCollision(balls[i],balls[j])){
+                //to calculate a unit vector along AB
+                Vector2 normal={balls[j]->pos.x-balls[i]->pos.x,balls[j]->pos.y-balls[i]->pos.y};
+                float len_normal=sqrtf(normal.x*normal.x+normal.y*normal.y);
+                normal.x/=len_normal;
+                normal.y/=len_normal;
+
+                Vector2 relative_velocity={balls[j]->velocity.x-balls[i]->velocity.x,balls[j]->velocity.y-balls[i]->velocity.y};
+                float overlap=balls[i]->radius+balls[j]->radius- 
+                                                sqrtf((balls[i]->pos.x - balls[j]->pos.x)*(balls[i]->pos.x - balls[j]->pos.x) 
+                                                + (balls[i]->pos.y - balls[j]->pos.y)*(balls[i]->pos.y - balls[j]->pos.y));
                 
+                float total_mass=balls[i]->mass+balls[j]->mass;
+                float coeffA=overlap*balls[j]->mass/total_mass;
+                float coeffB=overlap*balls[i]->mass/total_mass;
+
+                //correcting ball positions so they do not sink
+                //balls move distance proportional to the other balls mass
+                balls[i]->pos.x-=coeffA*normal.x;
+                balls[i]->pos.y-=coeffA*normal.y;
+                balls[j]->pos.x+=coeffB*normal.x;
+                balls[j]->pos.y+=coeffB*normal.y;
+
+                if (Vector2DotProduct(relative_velocity, normal) > 0) continue;
+                //correcting velocity using impulse
+                float impulse=-(1+1)*(normal.x*relative_velocity.x+normal.y*relative_velocity.y)*
+                                    (balls[i]->mass*balls[j]->mass)/total_mass;
+
+                balls[i]->velocity.x-=(impulse/balls[i]->mass)*normal.x;
+                balls[i]->velocity.y-=(impulse/balls[i]->mass)*normal.y;
+                balls[j]->velocity.x+=(impulse/balls[j]->mass)*normal.x;
+                balls[j]->velocity.y+=(impulse/balls[j]->mass)*normal.y;
+
+                balls[i]->color=RED;
 
             }
         }
@@ -103,18 +137,18 @@ int main(){
     int SCREEN_WIDTH=GetScreenWidth();
     int SCREEN_HEIGHT=GetMonitorHeight(0);
 
-    Ball* b1=new Ball(100,100,10,50,0);
 
     InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Physics Engine");
     vector<Ball*> balls;
-    balls.push_back(b1);
-    Ball* b2=new Ball(100,200,10,1100,0);
-    balls.push_back(b2);
-    Ball* b3=new Ball(100,250,10,100,1300); Ball* b4=new Ball(100,225,10,50,15);
-    balls.push_back(b3);balls.push_back(b4);
+    for(int i=0;i<1000;i++){
+
+        auto b= new Ball(GetRandomValue(50,900),GetRandomValue(50,900),10,GetRandomValue(-900,900),GetRandomValue(-900,900));
+        balls.push_back(b);
+    }
 
  
     SetExitKey(KEY_Q);
+   SetTargetFPS(60);
 
     while(!WindowShouldClose()){
         float dt=GetFrameTime();
